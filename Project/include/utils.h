6f79ac5c324be4ed0c4164bf7a86482a8dd92484
liviacardaccia02 <liviacardaccia99@gmail>
2024-04-22 12:01:08 +0200

    some new pins added

--- Project/include/utils.h
+++ Project/include/utils.h
@@ -4,13 +4,14 @@
 #define BOARDTYPE "ESP32"
 #define DHTTYPE DHT11
 #define FANPIN 1
-#define PWMPIN 2
+#define PWMFANPIN 2
 #define DHTPIN 4 
 #define MQ135PIN 5
-#define MQ7PIN 6
+#define MQ7PIN 14
+#define PWMMQ7PIN 13
 #define GREENLEDPIN 43
 #define REDLEDPIN 44
-#define R0_PRECISION 100 // TODO: to be removed (its already in SensorHandler.h)
-#define RatioMQ135CleanAir 3.6  //RS / R0 = 3.6 ppm of clean air
+#define VoltageResolution 5
+#define ADCBitResolution 12
 
 #endif
