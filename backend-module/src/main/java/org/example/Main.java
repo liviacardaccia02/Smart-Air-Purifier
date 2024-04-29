72c35d5876033d9e520023a83fff83acfd3ecaca
liviacardaccia02 <liviacardaccia99@gmail>
2024-04-29 11:44:32 +0200

    first implementation of mqtt client to receive messages on the topic Smart-air-purifier

--- backend-module/src/main/java/org/example/Main.java
+++ backend-module/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import com.hivemq.client.mqtt.MqttClient;
+import com.hivemq.client.mqtt.mqtt3.Mqtt3AsyncClient;
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Mqtt3AsyncClient client = MqttClient.builder()
+                .identifier(UUID.randomUUID().toString())
+                .serverHost("broker.hivemq.com")
+                .serverPort(1883)
+                .useMqttVersion3()
+                .buildAsync();
+
+        client.connect()
+                .thenAccept(connAck -> System.out.println("Connected successfully"))
+                .thenCompose(ack -> client.subscribeWith()
+                        .topicFilter("Smart-air-purifier")
+                        .callback(publish -> System.out.println("Received Message: " + new String(publish.getPayloadAsBytes())))
+                        .send())
+                .thenAccept(subAck -> System.out.println("Subscribed successfully"))
+                .exceptionally(ex -> {ex.printStackTrace(); return null;});
     }
 }
