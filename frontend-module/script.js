9a25cd8f6ea2c816896bd01b22ff9e2f0749f100
liviacardaccia02 <liviacardaccia99@gmail>
2024-05-07 16:58:28 +0200

    created a separeted file (probably to be removed)

---
+++ frontend-module/script.js
@@ -0,0 +1,75 @@
+function fetchData() {
+    fetch("http://localhost:8000/home"), {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+    }
+        .then(response => response.json())
+        .then(data => {
+            // Process the fetched data here
+            speed = data.speed;
+            document.getElementById('co-status').textContent = `${data.co} ppm`;
+            document.getElementById('co2-status').textContent = `${data.co2} ppm`;
+        })
+        .catch(error => {
+            // Handle any errors that occur during the fetch request
+            console.error(error);
+        });
+}
+
+function getCurrentDate() {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    const currentDate = new Date();
+    const dayOfWeek = days[currentDate.getDay()];
+    const month = months[currentDate.getMonth()];
+    const dayOfMonth = currentDate.getDate();
+    const suffix = getNumberSuffix(dayOfMonth);
+
+    return `${dayOfWeek}, ${month} ${dayOfMonth}${suffix}`;
+}
+
+function getNumberSuffix(dayOfMonth) {
+    if (dayOfMonth >= 11 && dayOfMonth <= 13) {
+        return 'th';
+    }
+    switch (dayOfMonth % 10) {
+        case 1: return 'st';
+        case 2: return 'nd';
+        case 3: return 'rd';
+        default: return 'th';
+    }
+}
+
+function drawChart() {
+    src = "https://cdn.jsdelivr.net/npm/chart.js";
+
+    const ctx = document.getElementById("energy-chart").getContext("2d");
+    const energyChart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"],
+            datasets: [{
+                label: "Energy Consumption",
+                data: [19, 22, 18, 25, 20],
+                fill: false,
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 2,
+            }],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+}
+
+window.onload = () => {
+    drawChart();
+}
